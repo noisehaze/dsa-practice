8591a21e2eed5441e89cb1791a41cb3337bce588
noisehaze <bhawnarathi411@gmail.com>
2026-01-17 17:06:41 +0530

    Initial commit

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
+
+    return 0;
+}
